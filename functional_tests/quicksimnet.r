24616768e73548c10c04622be67567aa851a5670
riccardocadei <riccardo.cadei98@gmail.com>
2023-03-31 18:05:35 +0200

    add functional test

--- functional_tests/quicksimnet.r
+++ functional_tests/quicksimnet.r
@@ -1,17 +1,10 @@
 rm(list=ls())
 library(statnet)
-setwd("~/Documents/IMT/Networks and ML/Script/Github")
-source("Effects.R")
-source("Variances.R")
-source("Probabilities.R")
-source("TreeSprouting.R")
-source("Others.R")
-source("PlotNCT.R")
-source("WholeNCT.R")
-
 library(stringi)
-
 library(igraph)
+library(ergm)
+
+
 #set the number of nodes
 N=4000
 m=40
@@ -23,17 +16,17 @@ x3=rbinom(N,1,0.3)
 x4=rbinom(N,1,0.6)
 X=cbind(x1,x2,x3,x4)
 
-#Generate m networks 
+#Generate m networks
 gsize=N/m
-library(ergm)
+
 
 #if N=1500 then param ==0.035, if N=3000 then param 0.01
 #if N=1500 then coefergm=c(2,-5.5), if N=3000 then coefergm=c(2,-7)
 ## Homophily based on x1
-simnet<-genmultnet(N=N,m=m,method="ergm",varhom=x1,param = NULL, coefergm=c(2,-5.5))
+simnet<-genmultnet(N=N,m=m,method="ergm",varhom=x1,param = NULL,coefergm=c(2,-5.5))
 #simnet<-genmultnet(N=N,m=m,method="er",varhom=x1,param = 0.01)
 
-#group Indicator 
+#group Indicator
 
 M=c(rep(1:m,gsize))
 M=sort(M)
@@ -63,7 +56,7 @@ isSymmetric(adiac_matrix)
 neigh<-rowSums(adiac_matrix)
 
 #Compute number of treated neigh and consequently Gi
-num_tr_neigh <-as.vector(adiac_matrix %*% treat) 
+num_tr_neigh <-as.vector(adiac_matrix %*% treat)
 neightreat=rep(1,N) #Gi
 neightreat[which(num_tr_neigh==0)]<-0
 
@@ -94,23 +87,23 @@ y = y0*(1-w) +  y1*w
 hist(y)
 
 
-NCT=NetworkCausalTrees(effweights<-c(1,0,0,0), 
-                 A<-adiac_matrix,p<-prt,fracpredictors<-1, 
+NCT=NetworkCausalTrees(effweights<-c(1,0,0,0),
+                 A<-adiac_matrix,p<-prt,fracpredictors<-1,
                  W<-w, Y<-y, X<-X,M<-M,  G<-g,
-                 Ne<-neigh, mdisc<-25, mest<-15, 
+                 Ne<-neigh, mdisc<-25, mest<-15,
                  minpopfrac<-1, depth<-3,
-                 minsize<-5, n_trees=1, 
+                 minsize<-5, n_trees=1,
                  method<-"singular",output<-"detection")
 
 title<-expression(paste("CAUSAL TREE TARGETED TO ",tau,"(1,0;0,0)"),sep="")
 plot.NetworkCausalTrees(NCT=NCT,output="detection",vcolor=c("seagreen4","seagreen1","lightblue1","dodgerblue2")
                         ,vsize=28,vsize2=28,coloreff ="1000",
                         vshape="circle",vframecolor = "black",
-                        vlabelcolor = "black", vlabelcex =1, 
-                        vlabelfont = 1,  elabelcex = 0.7, 
+                        vlabelcolor = "black", vlabelcex =1,
+                        vlabelfont = 1,  elabelcex = 0.7,
                         varnames=c("x1","x2","x3","x4"),
                         elabelfamily = "sans", ecolor="black",
-                        ewidth=0.3, elabelcolor="black", ot=FALSE, colleg = "black", 
+                        ewidth=0.3, elabelcolor="black", ot=FALSE, colleg = "black",
                         coltitleleg = "black", font.main=1,
                         cex.main = 1,adj=1,col.main = "black",
                         title=title
