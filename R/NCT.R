ae987715e23ef708df27356116742211009d5278
riccardocadei <riccardo.cadei98@gmail.com>
2023-05-24 16:36:26 -0400

    general code cleaning

--- R/NCT.R
+++ R/NCT.R
@@ -67,21 +67,21 @@
 #'
 #' @export
 #'
-NetworkCausalTrees=function(X, Y, W,
-                            effweights = c(0.25,0.25,0.25,0.25),
-                            A = NULL,
-                            G = NULL,
-                            M = NULL,
-                            p = NULL,
-                            mdisc = 25,
-                            mest = 15,
-                            minpopfrac = 1,
-                            fracpredictors = 1,
-                            n_trees = 1,
-                            depth = 3,
-                            minsize = 10,
-                            method = "singular",
-                            output = "estimation"){
+NetworkCausalTrees <- function(X, Y, W,
+                               effweights = c(1,0,0,0),
+                               A = NULL,
+                               G = NULL,
+                               M = NULL,
+                               p = NULL,
+                               mdisc = 25,
+                               mest = 15,
+                               minpopfrac = 1,
+                               fracpredictors = 1,
+                               n_trees = 1,
+                               depth = 3,
+                               minsize = 10,
+                               method = "singular",
+                               output = "estimation"){
 
   N <- length(W)
   m <- length(unique(M))
@@ -94,91 +94,120 @@ NetworkCausalTrees=function(X, Y, W,
   gamma <-effweights[3]
   delta <- effweights[4]
 
-
-  #error messages
-  if(alpha+beta+gamma+delta!=1){
-    stop('weights have to sum up to one')
+  # check input parameters
+  if (alpha+beta+gamma+delta!=1) {
+    stop('Effect weights (effweights) have to sum up to one.')
   }
 
-  if((length(which(effweights>0))>1) & (method=="singular" | method=="penalized")){
-    stop('if method is set to singular only one effect should have positive weight')
+  if ((length(which(effweights>0))>1) & (method=="singular" | method=="penalized")) {
+    stop('If method is set to singular only one effect should have positive weight.')
   }
 
-  if(1 %in% effweights & method=="composite"){
-    stop('composite gof is computed if at least two effects are investigated')
+  if (1 %in% effweights & method=="composite") {
+    stop('Composite gof is computed if at least two effects are investigated.')
   }
 
-  if(is.null(G) & is.null(A)){
-    stop('independently of the output, you have to specify either the G vector or the Adiacency Matrix')
+  if (is.null(G) & is.null(A)) {
+    stop('Independently of the output, you have to specify either the
+         Neighborhood Treatment vector G or the Adiacency Matrix A.')
   }
 
-  if(is.null(Ne) & is.null(A)){
-    stop('independently of the output, you have to specify either the Degree vector or the Adiacency Matrix')
+  if (is.null(Ne) & is.null(A)) {
+    stop('Independently of the output, you have to specify either the degree vector Ne
+         or the Adiacency Matrix A.')
   }
 
-  if(output=="estimation" & is.null(A)){
-    stop('if output is set estimation you have to specify the adiacency matrix')
+  if (output=="estimation" & is.null(A)) {
+    stop('If output is set estimation you have to specify the Adiacency Matrix A.')
   }
 
-  if(is.null(Ne)){
-    Ne<-rowSums(A)
+  if (is.null(Ne)) {
+    #stop('If you dont input the Adiacency Matrix you must specify the degree vector')
+    Ne <-rowSums(A)
   }
 
-
-  if(is.null(G)){
-    nt <-as.vector(A %*% W)
-    G=rep(1,N)
-    G[nt==0]<-0}
-
-  Nel<-vector(mode = "list", length = N)
-  for( i in  1:N){
-    Nel[[i]]<-which(A[i,]>0)
+  if (is.null(G)) {
+    nt <- as.vector(A %*% W)
+    G = rep(1,N)
+    G[nt==0] <- 0
   }
 
-  if(is.null(Ne)){
-    stop('if you dont input the Adiacency Matrix you must specify the degree vector')
+  Nel <- vector(mode = "list", length = N)
+  for (i in  1:N) {
+    Nel[[i]] <- which(A[i,]>0)
   }
 
 
+  Peff <- popeff(N=N,W=W,G=G,Y=Y,p=p,Ne=Ne)
+  sampgroup_train <- sample(1:m,size=mdisc,replace=FALSE)
+  trees <- plyr::raply(n_trees,
+                       sproutnetctree(method = method,
+                                      sampgroup = sampgroup_train,
+                                      fracpredictors = fracpredictors,
+                                      m = m,
+                                      minpopfrac = minpopfrac,
+                                      depth = depth,
+                                      minsize = minsize,
+                                      alpha = alpha,
+                                      beta = beta,
+                                      gamma = gamma,
+                                      delta = delta,
+                                      N = N,
+                                      W = W,
+                                      G = G,
+                                      Y = Y,
+                                      X = X,
+                                      M = M,
+                                      Ne = Ne,
+                                      p = p,
+                                      Peff = Peff,
+                                      Nel = Nel),
+                       .progress = "text" )
 
-  data <- data.frame(idunit=1:N,W=W,G=G,Y=Y,X=X,M=M)
-
-  Peff<-popeff(N=N,W=W,G=G,Y=Y,p=p,Ne=Ne)
-
-  sampgroup_train=sample(1:m,size=mdisc,replace=FALSE)
-
-  trees <- plyr::raply(
-    n_trees,
-    sproutnetctree(method=method,sampgroup=sampgroup_train,fracpredictors=fracpredictors,m=m,minpopfrac=minpopfrac,
-                   depth=depth,minsize=minsize,alpha=alpha,beta=beta,gamma=gamma,delta=delta,
-                   N=N,W=W,G=G,Y=Y,X=X,M=M,Ne=Ne,p=p,Peff=Peff,Nel=Nel),
-    .progress = "text" )
-
-  Forest<-data.frame(IDTREE=NA,NODE = NA, GOF=NA, NOBS = NA, FILTER = NA, TERMINAL = NA,
-                     stringsAsFactors = FALSE)
-
+  Forest <- data.frame(IDTREE = NA,
+                       NODE = NA,
+                       GOF = NA,
+                       NOBS = NA,
+                       FILTER = NA,
+                       TERMINAL = NA,
+                       stringsAsFactors = FALSE)
 
   for(i in 1:n_trees){
-    tree=NA
-    tree<-cbind(rep(i,nrow(as.data.frame(trees[i]))),as.data.frame(trees[i]))
-    colnames(tree)<-colnames(Forest)
-    Forest<-rbind(Forest,tree)
+    tree <- NA
+    tree <- cbind(rep(i,nrow(as.data.frame(trees[i]))),
+                  as.data.frame(trees[i]))
+    colnames(tree) <- colnames(Forest)
+    Forest <- rbind(Forest,tree)
   }
 
   Forest=Forest[-1,]
 
-  Results<-data.frame(GOF=Forest$GOF,NOBS=Forest$NOBS ,  FILTER =  c("NA",as.vector(na.omit(unique(Forest$FILTER)))),
-                      NUMTREE=NA, IDTREE=Forest$IDTREE, stringsAsFactors = FALSE)
+  Results<-data.frame(GOF = Forest$GOF,
+                      NOBS = Forest$NOBS,
+                      FILTER =  c("NA",as.vector(na.omit(unique(Forest$FILTER)))),
+                      NUMTREE = NA,
+                      IDTREE = Forest$IDTREE,
+                      stringsAsFactors = FALSE)
 
   for(j in as.vector(na.omit(unique(Forest$FILTER)))){
-    Results$NUMTREE[Results$FILTER==j]<-length(unique(Forest$IDTREE[which(Forest$FILTER==j)]))
-    Results$NUMTREE[Results$FILTER=="NA"]=length(unique(Forest$IDTREE[is.na(Forest$FILTER)]))
+    Results$NUMTREE[Results$FILTER==j] <- length(unique(Forest$IDTREE[which(Forest$FILTER==j)]))
+    Results$NUMTREE[Results$FILTER=="NA"] <- length(unique(Forest$IDTREE[is.na(Forest$FILTER)]))
   }
 
   if(nrow(Results)==1){
     warning('No split has been made')
   }
 
-  Results_est<-alleffect(output=output,tree_info = Results,N=N,W=W,G=G,Y=Y,X=X,Ne=Ne,p=p,Nel=Nel,minsize=minsize)
-  return(Results_est)
+  results_est <- alleffect(output = output,
+                           tree_info = Results,
+                           N = N,
+                           W = W,
+                           G = G,
+                           Y = Y,
+                           X = X,
+                           Ne = Ne,
+                           p = p,
+                           Nel = Nel,
+                           minsize = minsize)
+  return(results_est)
 }
--- R/TreeSprouting.R
+++ R/TreeSprouting.R
@@ -2,7 +2,7 @@
 #' Objective Function (OF)
 #'
 #' @description
-#' Computes the measure of the  Objective Function
+#' Computes the measure of the Objective Function
 #'
 #' @param method method to compute the Objective function: "singular" for NCT targeted to one single effect;
 #' "composite" for NCT targeted to multiple effects; "penalized" for a OF computed while
@@ -11,15 +11,15 @@
 #' @param beta weight associated to the effect 1101
 #' @param gamma weight associated to the effect 1110
 #' @param delta weight associated to the effect 0100
-#' @param  N Sample size
-#' @param  W N x 1 vector, Individual Treatment
-#' @param  G N x 1 vector, Neighborhood Treatment
-#' @param  Y N x 1 vector, Observed Outcome
-#' @param  p  N x 1 vector,Probability to be assigned to the active individual intervention
-#' @param  Ne N x 1 vector, Degree
-#' @param  Nel List of N elements - where N is the sample size -
+#' @param N Sample size
+#' @param W N x 1 vector, Individual Treatment
+#' @param G N x 1 vector, Neighborhood Treatment
+#' @param Y N x 1 vector, Observed Outcome
+#' @param p  N x 1 vector,Probability to be assigned to the active individual intervention
+#' @param Ne N x 1 vector, Degree
+#' @param Nel List of N elements - where N is the sample size -
 #' where each element i contains the IDs of the direct neighbors of unit i
-#' @param  Peff 4 dimensional vector containing the estimated effects in the
+#' @param Peff 4 dimensional vector containing the estimated effects in the
 #' whole population
 #' @param vartot - to be included if method = "penalized" - whole variance
 #' @param leafs - to be included if method = "penalized" - number of leafs
@@ -82,16 +82,16 @@ OF_Value=function(method,alpha,beta,gamma,delta,N,W,G,Y,p,Ne,Nel,Peff,vartot,lea
 #' @param beta weight associated to the effect 1101
 #' @param gamma weight associated to the effect 1110
 #' @param delta weight associated to the effect 0100
-#' @param  N Sample size
-#' @param  W N x 1 vector, Individual Treatment
-#' @param  G N x 1 vector, Neighborhood Treatment
-#' @param  Y N x 1 vector, Observed Outcome
-#' @param  X N x K matrix, Observed Covariate Matrix
-#' @param  p  N x 1 vector, Probability to be assigned to the active individual intervention
-#' @param  Ne N x 1 vector, Degree
-#' @param  Nel List of N elements - where N is the sample size -
+#' @param N Sample size
+#' @param W N x 1 vector, Individual Treatment
+#' @param G N x 1 vector, Neighborhood Treatment
+#' @param Y N x 1 vector, Observed Outcome
+#' @param X N x K matrix, Observed Covariate Matrix
+#' @param p  N x 1 vector, Probability to be assigned to the active individual intervention
+#' @param Ne N x 1 vector, Degree
+#' @param Nel List of N elements - where N is the sample size -
 #' where each element i contains the IDs of the direct neighbors of unit i
-#' @param  Peff 4 dimensional vector containing the estimated effects in the
+#' @param Peff 4 dimensional vector containing the estimated effects in the
 #' whole population
 #' @param vartot - to be included if method = "penalized" - whole variance
 #' @param leafs - to be included if method = "penalized" - number of leafs
@@ -503,7 +503,6 @@ alleffect=function(output,tree_info,N,W,G,Y,X,Ne,Nel,p,minsize){
     Results_tree_info<-rbind(Results_tree_info,tree_info)
   }
 
-  return(  Results_tree_info)
-
+  return(Results_tree_info)
 }
 
