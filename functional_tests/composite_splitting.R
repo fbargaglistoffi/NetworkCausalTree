e44bc6ca605f85e44874593b4376b7779973a712
Costanza Tortù <35844170+costitortu@users.noreply.github.com>
2023-10-20 09:13:32 +0200

    Add files via upload

--- functional_tests/composite_splitting.R
+++ functional_tests/composite_splitting.R
@@ -8,19 +8,22 @@ dataset <- data_generator(N = 2000,
                           h = 0,
                           method_networks = "sf")
 
-result <- NetworkCausalTrees(X =  dataset[["X"]],
-                             Y = dataset[["Y"]],
-                             W = dataset[["W"]],
-                             A = dataset[["A"]],
-                             M = dataset[["M"]],
-                             p = dataset[["p"]],
-                             effweights = c(0.25,0.25,0.25,0.25),
-                             ratio_dis = 0.5,
-                             depth = 3,
-                             minsize = 5,
-                             method = "composite",
-                             output = "estimation")
+result <- NetworkCausalTree(X =  dataset[["X"]],
+                            Y = dataset[["Y"]],
+                            W = dataset[["W"]],
+                            A = dataset[["A"]],
+                            M = dataset[["M"]],
+                            p = dataset[["p"]],
+                            effect_weights =   c(0.25,0.25,0.25,0.25),
+                            ratio_disc = 0.5,
+                            depth = 3,
+                            minsize = 5,
+                            method = "composite",
+                            output = "estimation")
 
 title <- expression("CAUSAL TREE TARGETED TO ALL THE EFFECTS")
 cov_names <- colnames(dataset[["X"]])
-plot_NCT(result, cov_names, title)
+
+plot_NCT(result, 
+         cov_names, 
+         title)
--- functional_tests/singular_splitting.R
+++ functional_tests/singular_splitting.R
@@ -1,28 +1,30 @@
 set.seed(1)
 
-dataset <- data_generator(N = 2000,
+dataset <- data_generator(N = 2000, 
                           K = 4,
-                          m = 40,
-                          p = rep(0.2,2000),
-                          het = TRUE,
-                          h = 2,
-                          method_networks = "er",
+                          m = 40, 
+                          p = rep(0.2,2000), 
+                          het = TRUE, 
+                          h = 2, 
+                          method_networks = "er", 
                           param_er = 0.1)
 
-result <- NetworkCausalTrees(X =  dataset[["X"]],
-                             Y = dataset[["Y"]],
-                             W = dataset[["W"]],
-                             A = dataset[["A"]],
-                             M = dataset[["M"]],
-                             p = dataset[["p"]],
-                             effweights = c(1,0,0,0),
-                             ratio_dis = 0.5,
-                             depth = 3,
-                             minsize = 5,
-                             method = "singular",
-                             output = "estimation")
+result <- NetworkCausalTree(X = dataset[["X"]],
+                            Y = dataset[["Y"]],
+                            W = dataset[["W"]], 
+                            A = dataset[["A"]],
+                            M = dataset[["M"]],
+                            p = dataset[["p"]], 
+                            effect_weights = c(1,0,0,0),
+                            ratio_dis = 0.5,
+                            depth = 3,
+                            minsize = 5, 
+                            method = "singular",
+                            output = "estimation")
 
-
-title <- expression(paste("CAUSAL TREE TARGETED TO ",tau,"(1,0;0,0)",sep=""))
+title <- expression(paste("CAUSAL TREE TARGETED TO ",tau,"(1,0;0,0)"),sep="")
 cov_names <- colnames(dataset[["X"]])
-plot_NCT(result, cov_names, title)
+
+plot_NCT(NCT = result, 
+         cov_names = cov_names,
+         title = title)
