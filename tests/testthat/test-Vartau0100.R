7e14d312cb7a931c642f38ad19a217dd7ca109b2
charliewang123 <charliewang123@g.ucla.edu>
2025-11-14 18:30:51 -0800

    Create test-Vartau0100.R

---
+++ tests/testthat/test-Vartau0100.R
@@ -0,0 +1,15 @@
+test_that("Vartau0100 runs without errors", {
+  set.seed(100)
+  
+  N  <- 6
+  W  <- c(0,1,0,1,0,1)
+  G  <- c(0,0,1,1,0,1)
+  Y  <- 1 + W + G + rnorm(N, 0, 0.05)
+  p  <- rep(0.5, N)
+  Ne <- rep(1, N)
+  Ne_list <- list(2,1,4,3,6,5)
+  
+  res <- Vartau0100(N,Y,W,G,p,Ne,Ne_list)
+  
+  expect_true(is.numeric(res) || is.na(res))
+})
