a7b657863278cdecc8e53188fcee18f60a3dcf7f
charliewang123 <charliewang123@g.ucla.edu>
2026-03-13 10:01:41 -0700

    Update Vartau0100.R

--- R/Vartau0100.R
+++ R/Vartau0100.R
@@ -3,14 +3,13 @@
 #'
 #' @description
 #' Computes the estimated variance of the effect 0100. This effect
-#'  compares individuals with the joint treatment exposure (w=0,g=1)
+#' compares individuals with the joint treatment exposure (w=0,g=1)
 #' with those with (w=0,g=0)
 #'
-#'
 #' @param  N Sample size
+#' @param  Y N x 1 vector, Observed Outcome
 #' @param  W N x 1 vector, Individual Treatment
 #' @param  G N x 1 vector, Neighborhood Treatment
-#' @param  Y N x 1 vector, Observed Outcome
 #' @param  p N x 1 vector, Probability to be assigned to the active individual intervention
 #' @param  Ne N x 1 vector, Degree
 #' @param  Ne_list List of N elements - where N is the sample size -
@@ -23,23 +22,25 @@ Vartau0100 = function(N, Y, W, G,
                       p, Ne, Ne_list) {
   
   # Variance of the Average Potential Outcome Y(w,g)=Y(0,1)
-  vary01 <- Vary(w = 0, g = 1, N = N, W = W, G = G,
+  vary01 <- Vary(w = 0, g = 1, W = W, G = G,
                  p = p, Ne = Ne, Ne_list = Ne_list, Y = Y)
   
   # Variance of the Average Potential Outcome Y(w,g)=Y(0,0)
-  vary00 <- Vary(w = 0, g = 0, N = N, W = W, G = G,
+  vary00 <- Vary(w = 0, g = 0, W = W, G = G,
                  p = p, Ne = Ne, Ne_list = Ne_list, Y = Y)
   
   # Covariance of the Average Potential Outcomes Y(w,g)=Y(0,1) and Y(w,g)=Y(0,0) 
-  covy01y00 <- Covy(w1 = 0, g1 = 1, w2 = 0, g2 = 0,
-                    N = N, W = W, G = G, p = p,
+  covy01y00 <- Covy(N = N, w1 = 0, g1 = 1, w2 = 0, g2 = 0, W = W, G = G, p = p,
                     Ne = Ne, Ne_list = Ne_list, Y = Y)
   
   if (any(is.na(c(vary01, vary00)))) {
     var0100 = NA
   } else {
-    var0100 = abs((1 / (N ^ (2))) *
-                    (vary01 + vary00 - 2 * covy01y00))
+    var0100 = (1 / (N ^ (2))) * (vary01 + vary00 - 2 * covy01y00)
+    if (var0100 < 0) {
+      warning("Estimated variance is negative; returning NA.")
+      var0100 <- NA
+    }
   }
   return(var0100)
 }
