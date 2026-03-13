aa9cee891b318762af7a8febe4e75557c115dbc2
charliewang123 <charliewang123@g.ucla.edu>
2026-03-13 10:01:47 -0700

    Update Vary.R

--- R/Vary.R
+++ R/Vary.R
@@ -5,13 +5,12 @@
 #' Computes the estimated variance of the average potential outcome
 #' related to a given level of the joint intervention - denoted with (w,g) -.
 #'
-#' @param  N Sample size
 #' @param  w Individual Treatment level
 #' @param  g Neighborhood Treatment level
+#' @param  Y N x 1 vector, Observed Outcome
 #' @param  W N x 1 vector, Individual Treatment
 #' @param  G N x 1 vector, Neighborhood Treatment
-#' @param  Y N x 1 vector, Observed Outcome
-#' @param  p Probability to be assigned to the active individual intervention
+#' @param  p N x 1 vector, Probability to be assigned to the active individual intervention
 #' @param  Ne N x 1 vector, Degree
 #' @param  Ne_list List of N elements - where N is the sample size -
 #' where each element i contains the IDs of the direct neighbors of unit i
@@ -20,17 +19,16 @@
 #'  average potential outcome under the level w,g of the joint intervention
 
 
-Vary = function(N, w, g, Y, W, G, 
+Vary = function(w, g, Y, W, G, 
                 p, Ne, Ne_list) {
-  
-  varzero = NULL
-  second_element = c()
-  
+
   if (length(which(W == w & G == g)) > 1) {
     
     pairs <- expand.grid.unique(which(W == w & G == g), 
                                 which(W == w & G == g),
                                 include.equals = FALSE)
+    
+    second_element = numeric(nrow(pairs))
     
     for (k in 1 : nrow(pairs)) { 
       
@@ -41,10 +39,11 @@ Vary = function(N, w, g, Y, W, G,
         
         # covariance correction term
         # accounts for correlation between connected individuals
-        second_element = c(varzero, sum(((pij(i, j, w, g, w, g, Ne, Ne_list, p = p)
-                         -pi(i, w, g, p, Ne) * pi(j, w, g, p, Ne)) /
-                         (pij(i, j, w, g, w, g, Ne, Ne_list, p = p))) *
-                         (Y[i] / pi(i, w, g, p, Ne)) * (Y[j] / pi(j, w, g, p,Ne))))
+        pij_val <- pij(i, j, w, g, w, g, Ne, Ne_list, p = p)
+        pi_i <- pi(i, w, g, p, Ne)
+        pi_j <- pi(j, w, g, p, Ne)
+        second_element[k] = ((pij_val - pi_i * pi_j) / pij_val) *
+          (Y[i] / pi_i) * (Y[j] / pi_j)
       }
       
       }
