9f99ba90394c73c68823acd7bb2dbab057cd8441
charliewang123 <charliewang123@g.ucla.edu>
2026-03-14 16:27:06 -0700

    Update test-plot_NCT.R

--- tests/testthat/test-plot_NCT.R
+++ tests/testthat/test-plot_NCT.R
@@ -2,7 +2,7 @@ test_that("plot_NCT runs on a real tiny NCT object", {
   skip_on_cran()
   
   set.seed(100)
-  d <- data_generator(N = 100, k = 5, M = 2, p = rep(0.3,100), remove_isolates = TRUE)
+  d <- data_generator_direct_indirect(N = 100, k = 5, M = 2, p = rep(0.3,100), remove_isolates = TRUE)
   
   fit <- NetworkCausalTree(
     Y = d$Y, W = d$W, X = d$X, A = d$A, p = d$p,
@@ -22,7 +22,7 @@ test_that("plot_NCT works in detection mode", {
   skip_on_cran()
   
   set.seed(100)
-  d <- data_generator(N = 100, k = 5, M = 2, p = rep(0.3,100), remove_isolates = TRUE)
+  d <- data_generator_direct_indirect(N = 100, k = 5, M = 2, p = rep(0.3,100), remove_isolates = TRUE)
   
   fit <- NetworkCausalTree(
     Y = d$Y, W = d$W, X = d$X, A = d$A, p = d$p,
